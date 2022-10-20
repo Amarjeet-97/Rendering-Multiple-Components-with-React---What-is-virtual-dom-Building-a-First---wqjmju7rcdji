70b118f658a7237921b165549eaad84201a1a5d4
Amarjeet-97 <amarjeetkumardmg8409@gmail.com>
2022-10-20 11:11:16 +0530

    Second Attempt to finish

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,8 @@ import "../styles/App.css";
 const App = () => {
   return (
     <div id="main">
-      <h1 id="data-ns-test" value="project-name">
-        Project Name
-      </h1>
-      <p id="data-ns-test" value="project-description">
-        Project Description
-      </p>
+      <h1 data-ns-test="project-name">Project Name</h1>
+      <p data-ns-test="project-description">Project Description</p>
     </div>
   );
 };
