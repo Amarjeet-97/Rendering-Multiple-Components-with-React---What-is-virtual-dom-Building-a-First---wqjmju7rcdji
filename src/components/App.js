07159224057f7b93a1f75f7f6afc0cd3c45d7204
Amarjeet-97 <amarjeetkumardmg8409@gmail.com>
2022-10-20 11:02:24 +0530

    Intial assignment completion test

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
   return (
-    <div id="main"></div>
-  )
-}
-
+    <div id="main">
+      <h1 id="data-ns-test" value="project-name">
+        Project Name
+      </h1>
+      <p id="data-ns-test" value="project-description">
+        Project Description
+      </p>
+    </div>
+  );
+};
 
 export default App;
